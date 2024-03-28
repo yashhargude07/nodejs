0cdff60c1150ff1f83a7e24f5b66eadce36dbbee
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-03-28 19:32:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-var circle = require('./circle.js');
-var obj = circle(7);
-var output = obj.area();
+const EventEmitter = require('events');
+
+var eventEmitter1 = new EventEmitter();
+
+var eventEmitter2 = new EventEmitter();
+
+console.log("Default max listener for eventEmitter1 is:",eventEmitter1.getMaxListeners());
+
+console.log("Default max listener for eventEmitter2 is:",eventEmitter2.getMaxListeners());
+
+EventEmitter.defaultMaxListeners = 2;
+
+console.log("Default max listener for eventEmitter1 is:",eventEmitter1.getMaxListeners());
+
+console.log("Default max listener for eventEmitter2 is:",eventEmitter2.getMaxListeners());
+
+eventEmitter1.setMaxListeners(5);
+
+console.log("Default max listener for eventEmitter1 is:",eventEmitter1.getMaxListeners());
+
+console.log("Default max listener for eventEmitter2 is:",eventEmitter2.getMaxListeners());
